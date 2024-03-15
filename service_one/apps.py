b4d5ed7b1f047f0124931dd2140e062727b0e03c
Sarvin Nami <srvn0nm@gmail.com>
2024-03-15 18:13:44 +0330

    apps

---
+++ service_one/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ServiceOneConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "service_one"
---
+++ service_two/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ServiceTwoConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "service_two"
